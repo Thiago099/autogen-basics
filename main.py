f810aa21c4f049f1cc64e438f5e806200fdf2398
Thiago Kaique <thiago.kaique@stayfilm.com.br>
2023-10-21 01:58:08 -0300

    refactor

---
+++ main.py
@@ -0,0 +1,22 @@
+from autogen import AssistantAgent, UserProxyAgent
+
+config_list = [
+    {
+        'model': 'open_ai',
+        'api_base': 'http://localhost:1234/v1',
+        'api_key': 'null'
+    }
+]
+
+llm_config={
+    "request_timeout": 600,
+    "seed": 42,
+    "config_list": config_list,
+    "temperature": 0
+}
+
+
+assistant = AssistantAgent("assistant", llm_config=llm_config)
+user_proxy = UserProxyAgent("user_proxy", code_execution_config={"work_dir": "coding"})
+
+user_proxy.initiate_chat(assistant, message="Plot a chart of NVDA and TESLA stock price change YTD.")
